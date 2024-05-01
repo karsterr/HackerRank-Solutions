5acb2ee81755fc21ac68ac3a366d19bd47300e0d
Efe Can Kara <efecan_kara@tuta.io>
2024-05-01 12:56:31 +0300

    Solve 30 Days of Code - 6

---
+++ 30 Days of Code/6 - Lets Review.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    
+    even = ""
+    odd = ""
+    
+    for i in range(len(s)):
+        if i % 2 == 0:
+            even += s[i]
+        else:
+            odd += s[i]
+    
+    print(even, odd)
